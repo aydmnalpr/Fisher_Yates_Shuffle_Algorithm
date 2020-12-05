8303f7b788f567b5242fc37958bace805795daa2
Alper Ayduman <aydumanalper@gmail.com>
2020-12-05 17:00:24 +0300

    Initial commit

---
+++ Fisher_Yates.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//The Fisher-Yates Shuffle Algorithm
+public static class Fisher_Yates 
+{
+    public static T[] ShuffleArray<T>(T[] array, int seed)
+    {
+        System.Random randomGenerator = new System.Random(seed);
+
+        //last iteration will be ignored because of the algorithm
+        for (int i = 0; i < array.Length-1; i++)
+        {
+            int randomIndex = randomGenerator.Next(i, array.Length);
+            T tempItem = array[randomIndex];
+            array[randomIndex] = array[i];
+            array[i] = tempItem;
+        }
+
+        return array;
+    }
+}
